df8cf03b4e7bc46c4522592d9a3f9a7d5e8c2edc
Tomasz Rewak <tomaszrewak@gmail.com>
2019-03-09 17:58:39 +0100

    Experimenting with noise.

--- Rain.Sandbox/Program.cs
+++ Rain.Sandbox/Program.cs
@@ -10,12 +10,6 @@ namespace Rain.Sandbox
 	{
 		private int _sample;
 
-		public SimpleWaveProvider()
-		{
-			Frequency = 1000;
-			Amplitude = 0.25f; // let's not hurt our ears            
-		}
-
 		public float Frequency { get; set; }
 		public float Amplitude { get; set; }
 		public WaveFormat WaveFormat { get; set; }
@@ -32,26 +26,52 @@ namespace Rain.Sandbox
 			}
 			return count;
 		}
+	}
 
+	public class WhiteNoiseWaveProvider : IWaveProvider
+	{
+		private float _lastSample;
+		private Random _randomGenerator = new Random();
+
+		public float Frequency { get; set; }
+		public float Amplitude { get; set; }
+		public WaveFormat WaveFormat { get; set; }
+
+		public int Read(byte[] buffer, int offset, int count)
+		{
+			var valueBuffer = MemoryMarshal.Cast<byte, float>(buffer.AsSpan().Slice(offset, count));
+
+			int sampleRate = WaveFormat.SampleRate;
+			for (int n = 0; n < valueBuffer.Length; n++)
+			{
+				float sample = (float)_randomGenerator.NextDouble();
+
+				_lastSample = _lastSample + (float)Math.Max(-0.1, Math.Min(0.1, Math.Sign(sample - _lastSample) * (float)Math.Pow(sample - _lastSample, 2)));
+
+				valueBuffer[n] = (float)(Amplitude * _lastSample);
+			}
+			return count;
+		}
 	}
 
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			var sineWaveProvider = new SimpleWaveProvider();
-			sineWaveProvider.WaveFormat = WaveFormat.CreateIeeeFloatWaveFormat(16000, 1); // 16kHz mono
-			sineWaveProvider.Frequency = 500;
-			sineWaveProvider.Amplitude = 0.2f;
+			var sineWaveProvider = new WhiteNoiseWaveProvider
+			{
+				Frequency = 500,
+				Amplitude = 0.2f,
+				WaveFormat = WaveFormat.CreateIeeeFloatWaveFormat(16000, 1)
+			};
 
 			using (var wo = new WaveOutEvent())
 			{
 				wo.Init(sineWaveProvider);
 				wo.Play();
+
 				while (wo.PlaybackState == PlaybackState.Playing)
-				{
 					Thread.Sleep(500);
-				}
 			}
 		}
 	}

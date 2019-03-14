51fc423da0a5df86ebefbe06bdf980819a1e4328
Tomasz Rewak <tomaszrewak@gmail.com>
2019-03-14 22:11:39 +0100

    Triangle and square wave generators.

---
+++ Rain.Wave/Generators/SquareWaveGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rain.Wave.Generators
+{
+	public sealed class SquareWaveGenerator : IWave
+	{
+		public float Probe(float time)
+		{
+			return time % 1f < 0.5 ? -1f : 1f;
+		}
+	}
+}
---
+++ Rain.Wave/Generators/TriangleWaveGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rain.Wave.Generators
+{
+	public sealed class TriangleWaveGenerator : IWave
+	{
+		public float Probe(float time)
+		{
+			var modulo = time % 1f;
+
+			return
+				modulo < 0.5f
+				?
+				4f * modulo - 1f
+				:
+				-4f * modulo + 3f;
+		}
+	}
+}

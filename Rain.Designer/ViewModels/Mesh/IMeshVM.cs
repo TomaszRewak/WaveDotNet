fbb436d7e6e86c3afc830976f52f6a68ae43e3f1
Tomasz Rewak <tomaszrewak@gmail.com>
2019-03-17 21:24:56 +0100

    Planning view models and controls.

---
+++ Rain.Designer/ViewModels/Mesh/IMeshVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rain.Designer.ViewModels.Mesh
+{
+	internal interface IMeshVM
+	{
+	}
+}
---
+++ Rain.Designer/ViewModels/Mesh/Implementation/MeshVM.cs
@@ -0,0 +1,23 @@
+﻿using Rain.Designer.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rain.Designer.ViewModels.Mesh.Implementation
+{
+	internal class MeshVM : ViewModel, IMeshVM
+	{
+		public IMeshNodeVM[,] Nodes { get; private set; }
+
+		public MeshVM()
+		{
+			Nodes = new[,] {
+				{ new MeshNodeVM() }
+			};
+		}
+
+
+	}
+}

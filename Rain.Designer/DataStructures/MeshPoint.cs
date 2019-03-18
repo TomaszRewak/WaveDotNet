8b6ee6a22a997c0bc02894c0bd4bbc979bcaeb19
Tomasz Rewak <tomaszrewak@gmail.com>
2019-03-18 19:56:38 +0100

    MeshPoint data structure.

---
+++ Rain.Designer/DataStructures/MeshPoint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rain.Designer.DataStructures
+{
+	internal struct MeshPoint
+	{
+		public int Row { get; set; }
+		public int Column { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			if (obj is MeshPoint value)
+				this.Equals(value);
+
+			return false;
+		}
+
+		public bool Equals(MeshPoint value)
+		{
+			return Row == value.Row && Column == value.Column;
+		}
+
+		public override int GetHashCode()
+		{
+			return Row.GetHashCode() ^ Column.GetHashCode();
+		}
+
+		public static bool operator == (MeshPoint lhs, MeshPoint rhs)
+		{
+			return lhs.Equals(rhs);
+		}
+
+		public static bool operator !=(MeshPoint lhs, MeshPoint rhs)
+		{
+			return !lhs.Equals(rhs);
+		}
+	}
+}
--- Rain.Designer/ViewModels/Mesh/Implementation/MeshNodeVM.cs
+++ Rain.Designer/ViewModels/Mesh/Implementation/MeshNodeVM.cs
@@ -1,4 +1,5 @@
-﻿using Rain.Designer.ViewModels.Common;
+﻿using Rain.Designer.DataStructures;
+using Rain.Designer.ViewModels.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,6 @@ namespace Rain.Designer.ViewModels.Mesh.Implementation
 {
 	internal class MeshNodeVM : ViewModel, IMeshNodeVM
 	{
+		public UnorderedPair<MeshPoint> Position { get; set; }
 	}
 }

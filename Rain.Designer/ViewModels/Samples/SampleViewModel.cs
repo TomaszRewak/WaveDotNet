1d462b1a0265044024ffb546303e127117ebe0ed
Tomasz Rewak <tomaszrewak@gmail.com>
2019-04-08 22:00:16 +0200

    Samples view models.

---
+++ Rain.Designer/ViewModels/Samples/SampleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Rain.Designer.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rain.Designer.ViewModels.Samples
+{
+    internal class SampleViewModel : ViewModel
+    {
+    }
+}

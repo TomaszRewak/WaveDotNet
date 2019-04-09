35be43dd150b27e9c8548d57d4fe82a092cc431d
Tomasz Rewak <tomaszrewak@gmail.com>
2019-04-09 20:40:58 +0200

    Adding and removing subtrees.

--- Rain.Designer/ViewModels/Tree/TreeViewModel.cs
+++ Rain.Designer/ViewModels/Tree/TreeViewModel.cs
@@ -4,19 +4,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Rain.Designer.ViewModels.Tree
 {
     internal class TreeViewModel : ViewModel
     {
-		public TreeViewModel()
-		{ }
+		private readonly Func<TreeViewModel> _treeFactory;
 
-		private IReadOnlyCollection<TreeViewModel> _subTrees;
+		public TreeViewModel(Func<TreeViewModel> treeFactory)
+		{
+			_treeFactory = treeFactory;
+		}
+
+		private IReadOnlyCollection<TreeViewModel> _subTrees = new List<TreeViewModel>();
 		public IReadOnlyCollection<TreeViewModel> SubTrees
 		{
 			get => _subTrees;
 			set => Set(ref _subTrees, value);
 		}
+
+		private void AddSubTree()
+		{
+			SubTrees = SubTrees
+				.Concat(new[] { _treeFactory() })
+				.ToList();
+		}
+
+		public ICommand AddSubTreeCommand => new Command(AddSubTree);
     }
 }

95a672adbea13ec9fb0446a1eca120411a4899eb
Tomasz Rewak <tomaszrewak@gmail.com>
2019-04-14 09:26:09 +0200

    Adding custom markups.

---
+++ Rain.Designer/ViewModels/Tree/TreeDesignerViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Rain.Designer.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Rain.Designer.ViewModels.Tree
+{
+    internal class TreeDesignerViewModel : ViewModel
+    {
+		public TreeViewModel Tree { get; }
+
+		public TreeDesignerViewModel(TreeViewModel tree)
+		{
+			Tree = tree;
+		}
+
+		private TreeViewModel _selectedSubTree;
+		public TreeViewModel SelectedSubTree
+		{
+			get => _selectedSubTree;
+			set => Set(ref _selectedSubTree, value);
+		}
+
+		private void SelectSubTree(TreeViewModel tree)
+		{
+			this.SelectedSubTree = tree;
+		}
+
+		public ICommand SelectTreeCommand => new Command<TreeViewModel>(SelectSubTree);
+    }
+}

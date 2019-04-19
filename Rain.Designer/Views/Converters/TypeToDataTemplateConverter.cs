7713eedaf03a9b39c003815abd2566935cd76034
Tomasz Rewak <tomaszrewak@gmail.com>
2019-04-19 10:48:08 +0200

    Simplifying DataTemplate generation.

---
+++ Rain.Designer/Views/Converters/TypeToDataTemplateConverter.cs
@@ -0,0 +1,29 @@
+﻿using Rain.Designer.Views.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Rain.Designer.Views.Converters
+{
+	internal class TypeToDataTemplateConverter : ValueConverter<Type, DataTemplate>
+	{
+		public override DataTemplate Convert(Type value)
+		{
+			if (value == null)
+				return new DataTemplate();
+
+			return new DataTemplate
+			{
+				VisualTree = new FrameworkElementFactory(value)
+			};
+		}
+
+		public override Type ConvertBack(DataTemplate value)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

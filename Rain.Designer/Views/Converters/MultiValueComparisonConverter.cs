927f7de76f4d081f2f4ffa37ab2139d520bfc6ec
Tomasz Rewak <tomaszrewak@gmail.com>
2019-04-19 22:05:24 +0200

    Highlighting selected node.

---
+++ Rain.Designer/Views/Converters/MultiValueComparisonConverter.cs
@@ -0,0 +1,24 @@
+﻿using Rain.Designer.Views.Common;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Rain.Designer.Views.Converters
+{
+	internal class MultiValueComparisonConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			return values.Distinct().Count() == 1;
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

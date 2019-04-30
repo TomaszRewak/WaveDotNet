fcf293fecc07b91d3fdf72c488c79de621a377f0
Tomasz Rewak <tomaszrewak@gmail.com>
2019-04-30 20:30:10 +0200

    Creating form template.

---
+++ Rain.Designer/Views/Waves/Controls/SimpleWaveBlockForm.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Rain.Designer.Views.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rain.Designer.Views.Waves.Controls
+{
+	internal class SimpleWaveBlockFormBase : UserControl<SimpleWaveBlockForm>
+	{ }
+
+	[ContentProperty(nameof(Input))]
+	internal class SimpleWaveBlockFormField : DependencyObject<SimpleWaveBlockFormField>
+	{
+		private static readonly DependencyProperty NameProperty = Register<string>(nameof(Name));
+		public string Name
+		{
+			get => GetValue<string>(NameProperty);
+			set => SetValue(NameProperty, value);
+		}
+
+		private static readonly DependencyProperty InputProperty = Register<UIElement>(nameof(Input));
+		public UIElement Input
+		{
+			get => GetValue<UIElement>(InputProperty);
+			set => SetValue(InputProperty, value);
+		}
+	}
+
+	[ContentProperty(nameof(Fields))]
+	internal partial class SimpleWaveBlockForm : SimpleWaveBlockFormBase
+	{
+		public SimpleWaveBlockForm()
+		{
+			InitializeComponent();
+
+			Fields = new List<SimpleWaveBlockFormField>();
+		}
+
+		private static readonly DependencyProperty FieldsProperty = Register<List<SimpleWaveBlockFormField>>(nameof(Fields));
+		public List<SimpleWaveBlockFormField> Fields
+		{
+			get => GetValue<List<SimpleWaveBlockFormField>>(FieldsProperty);
+			set => SetValue(FieldsProperty, value);
+		}
+	}
+}
